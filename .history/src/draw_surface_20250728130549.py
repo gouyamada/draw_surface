d77c8c3a5da3f005d9361286dbe9fb959efb63fb
gouyamada <gou.yamada@gmail.com>
2025-07-28 13:23:01 +0900

    初版コミット

---
+++ .history/src/draw_surface_20250728130549.py
@@ -0,0 +1,56 @@
+"""
+3次元曲面を描画するスクリプト
+"""
+from typing import cast
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+
+plt.rcParams["font.family"] = "Meiryo"
+plt.rcParams["font.size"] = 12
+
+
+def surface_setting(x ,y):
+    """
+    描画する曲面の関数を定義する関数
+    """
+    z = np.sin(np.sqrt(x**2 + y**2))
+    return z
+
+
+def draw_surface():
+    """
+    3次元曲面を描画する関数
+    """
+    fig = plt.figure(figsize=(10, 8))
+    ax = cast(Axes3D, fig.add_subplot(111, projection="3d"))
+
+    # x, y の範囲を設定する
+    x = np.linspace(-5, 5, 100)
+    y = np.linspace(-5, 5, 100)
+    X, Y = np.meshgrid(x, y)
+    Z = surface_setting(X, Y)
+
+    # 曲面を描画する
+    surf = ax.plot_surface(X, Y, Z, cmap="viridis", edgecolor="none")
+    ax.set_title("3次元曲面の描画", fontsize=16)
+    ax.set_xlabel("X軸")
+    ax.set_ylabel("Y軸")
+    ax.set_zlabel("Z軸")
+
+    # 曲面の色を設定する
+    fig.colorbar(surf, shrink=0.5, aspect=10)
+    fig.tight_layout
+
+    plt.show()
+
+def main():
+    """
+    メイン関数
+    """
+    draw_surface()
+
+
+if __name__ == "__main__":
+    main()
